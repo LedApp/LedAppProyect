71590bf7d4a50f0f79066508d3e23935135ab4ee
Mirian Reina <yiri.mr@gmail.com>
2017-02-22 10:50:57 +0100

    Added ViewHolder and Adapter

--- app/src/main/java/es/telefonica/talentum/ledapp/view/EosLabViewHolder.java
+++ app/src/main/java/es/telefonica/talentum/ledapp/view/EosLabViewHolder.java
@@ -1,6 +1,29 @@
 package es.telefonica.talentum.ledapp.view;
 
 
-public class EosLabViewHolder{
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import es.telefonica.talentum.ledapp.R;
+
+public class EosLabViewHolder extends RecyclerView.ViewHolder{
+
+    private TextView productName;
+    private ImageView productImage;
+
+
+
+
+    public EosLabViewHolder(View itemView) {
+        super(itemView);
+        productName= (TextView) itemView.findViewById(R.id.catalogo_row__row);
+        productImage= (ImageView) itemView.findViewById(R.id.catalogo_row__image_row);
+
+    }
+
+
+
 
 }
