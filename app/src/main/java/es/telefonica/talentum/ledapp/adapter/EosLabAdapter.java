0b1843bcab72fddd230d3cd1a9fbbb2fd1163801
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-02-24 09:20:15 +0100

    Modified Adapter and ViewHolder

--- app/src/main/java/es/telefonica/talentum/ledapp/adapter/EosLabAdapter.java
+++ app/src/main/java/es/telefonica/talentum/ledapp/adapter/EosLabAdapter.java
@@ -1,26 +1,43 @@
 package es.telefonica.talentum.ledapp.adapter;
 
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import es.telefonica.talentum.ledapp.Product;
+import es.telefonica.talentum.ledapp.R;
 import es.telefonica.talentum.ledapp.view.EosLabViewHolder;
 
 public class EosLabAdapter extends RecyclerView.Adapter<EosLabViewHolder>{
 
+    private List<Product> productList = new LinkedList<>();
+    private LayoutInflater inflater;
+
+    public EosLabAdapter(Context context, List<Product> productList) {
+        this.productList = productList;
+        inflater = LayoutInflater.from(context);
+    }
 
     @Override
     public EosLabViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.catalogo_row, parent, false);
+        EosLabViewHolder row = new EosLabViewHolder(view);
+        return row;
     }
 
     @Override
     public void onBindViewHolder(EosLabViewHolder holder, int position) {
-
+        holder.setProduct(productList.get(position), holder.itemView.getContext());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return productList.size();
     }
 }
