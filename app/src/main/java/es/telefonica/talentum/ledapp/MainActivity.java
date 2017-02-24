51fce189b2f0af69ea5c38d2fe67a727ef3765ca
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-02-24 09:22:32 +0100

    Unite all to display the products in the recycler view

--- app/src/main/java/es/telefonica/talentum/ledapp/MainActivity.java
+++ app/src/main/java/es/telefonica/talentum/ledapp/MainActivity.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -19,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.clinica_dental) ImageButton clinicaDental;
     @BindView(R.id.medico) ImageButton medico;
 
-
+    private List<Product> productList = new LinkedList<>();
+    private List<Product> productListFinal = new LinkedList<>();
 
 
     @Override
@@ -28,10 +33,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        consultProduct();
+
         farmacia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getFarmacia();
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
+                i.putExtra("productList", (Serializable) productListFinal);
                 startActivity(i);
             }
         });
@@ -39,7 +48,9 @@ public class MainActivity extends AppCompatActivity {
         veterinaria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getVeterinarias();
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
+                i.putExtra("productList", (Serializable) productListFinal);
                 startActivity(i);
             }
         });
@@ -48,14 +59,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
-                startActivity(i);
+                i.putExtra("productList", (Serializable) productListFinal);
             }
         });
 
         ortopedia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getOrtopedia();
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
+                i.putExtra("productList", (Serializable) productListFinal);
                 startActivity(i);
             }
         });
@@ -63,7 +76,9 @@ public class MainActivity extends AppCompatActivity {
         parafarmicia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getParafarmacia();
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
+                i.putExtra("productList", (Serializable) productListFinal);
                 startActivity(i);
             }
         });
@@ -71,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
         clinicaDental.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getDentista();
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
+                i.putExtra("productList", (Serializable) productListFinal);
                 startActivity(i);
             }
         });
@@ -79,13 +96,93 @@ public class MainActivity extends AppCompatActivity {
         medico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                getMedico();
                 Intent i = new Intent(MainActivity.this, RecyclerView_Activity.class);
+                i.putExtra("productList", (Serializable) productListFinal);
                 startActivity(i);
             }
         });
     }
 
+    public void consultProduct(){
+        final ProductApi productApi = new ProductApi();
 
+        productApi.getProduct(this);
+
+        productApi.setOnProductDowloadDataFinish(new ProductApi.ProductDownloadDataFinish() {
+            @Override
+            public void newProduct(List<Product> productListApi) {
+
+                productList = productListApi;
+
+            }
+        });
+    }
+
+    public void getFarmacia(){
+        productListFinal = new LinkedList<>();
+        for (Product p:productList) {
+          if (p.getCategoria().equals("farmacia")){
+              productListFinal.add(p);
+          }
+
+        }
+
+    }
+    public void getVeterinarias(){
+        productListFinal = new LinkedList<>();
+        for (Product p:productList) {
+            if (p.getCategoria().equals("veterinario")){
+                productListFinal.add(p);
+            }
+
+        }
+
+    }
+
+    public void getOrtopedia(){
+        productListFinal = new LinkedList<>();
+        for (Product p:productList) {
+            if (p.getCategoria().equals("ortopedia")){
+                productListFinal.add(p);
+            }
+
+        }
+
+    }
+
+    public void getParafarmacia(){
+        productListFinal = new LinkedList<>();
+        for (Product p:productList) {
+            if (p.getCategoria().equals("parafarmacia")){
+                productListFinal.add(p);
+            }
+
+        }
+
+    }
+
+    public void getDentista(){
+        productListFinal = new LinkedList<>();
+        for (Product p:productList) {
+            if (p.getCategoria().equals("dentista")){
+                productListFinal.add(p);
+            }
+
+        }
+
+    }
+
+    public void getMedico(){
+        productListFinal = new LinkedList<>();
+        for (Product p:productList) {
+            if (p.getCategoria().equals("médico")){
+                productListFinal.add(p);
+            }
+
+        }
+
+    }
 
 
 
--- app/src/main/java/es/telefonica/talentum/ledapp/RecyclerView_Activity.java
+++ app/src/main/java/es/telefonica/talentum/ledapp/RecyclerView_Activity.java
@@ -1,13 +1,32 @@
 package es.telefonica.talentum.ledapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import es.telefonica.talentum.ledapp.adapter.EosLabAdapter;
 
 public class RecyclerView_Activity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_);
+
+        recyclerView = (RecyclerView) findViewById(R.id.activity_recycler_view_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Intent intent = this.getIntent();
+        List<Product> products = (List<Product>) intent.getSerializableExtra("productList");
+
+        EosLabAdapter adapter = new EosLabAdapter(this, products);
+        if (products != null){
+            recyclerView.setAdapter(adapter);
+        }
+
+
     }
 }
