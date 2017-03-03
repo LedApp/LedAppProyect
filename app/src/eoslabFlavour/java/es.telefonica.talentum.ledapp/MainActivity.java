46dd8de71554a5a4c5417d4dab86788f74aed016
Ruben <rubenmarin94i@gmail.com>
2017-03-03 09:52:39 +0100

    Added links to phone web site and mail

--- app/src/eoslabFlavour/java/es.telefonica.talentum.ledapp/MainActivity.java
+++ app/src/eoslabFlavour/java/es.telefonica.talentum.ledapp/MainActivity.java
@@ -1,12 +1,14 @@
 package es.telefonica.talentum.ledapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
         @BindView(R.id.main_view__catalogo) Button catalogo;
         @BindView(R.id.main_view__about_us) ImageButton information;
-
+        @BindView(R.id.main_view__web) TextView web;
+        @BindView(R.id.main_view__phone) TextView phone;
+        @BindView(R.id.main_view__correo) TextView correo;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +50,32 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
+            web.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("http://www.eoslab.com/");
+                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
 
+            phone.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("tel:" + "691494242");
+                    Intent intent = new Intent (Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
+                }
+            });
+
+            correo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Uri uri = Uri.parse("https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#identifier");
+                    Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
+                }
+            });
 
 
 
